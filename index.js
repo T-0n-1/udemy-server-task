a6699fda9f6ebe1f43641f4f3a92cff9361f9b7e
Toni Mertanen <t.mertanen@hotmail.com>
2023-12-08 16:00:06 +0200

    Added some navi elements for switching pages

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>")
+    res.send(`
+        <h1>Hello World</h1>
+        <ul>
+            <li><a href="/about">About Me</a></li>
+            <li><a href="/contact">Contact information</a></li>
+        </ul>
+        `)
 });
 app.get("/about", (req, res) => {
-    res.send("<h1>About Me</h1>")
+    res.send(`
+        <h1>About Me</h1>
+        <ul>
+            <li><a href="/contact">Contact information</a></li>
+            <li><a href="/">Back</a></li>
+        </ul>
+        `)
 });
 app.get("/contact", (req, res) => {
-    res.send("<h1>Contact Me</h1>")
+    res.send(`
+        <h1>Contact Me</h1>
+        <ul>
+            <li><a href="/about">About Me</a></li>
+            <li><a href="/">Back</a></li>
+        </ul>
+        `)
 });
 
 app.listen(port, () => {    
