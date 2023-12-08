224c8e838ff80fa65fcbdb70da7a4c0e20a48503
Toni Mertanen <t.mertanen@hotmail.com>
2023-12-08 15:45:17 +0200

    Multipage handling with node and express

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("<h1>Hello World</h1>")
+});
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1>")
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1>")
 });
 
 app.listen(port, () => {    
